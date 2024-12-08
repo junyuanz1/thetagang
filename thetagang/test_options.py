34ab879b1e301480d80584b1036541b4bb6926b6
Junyuan Zheng <53073448+junyuanz1@users.noreply.github.com>
2024-12-07 16:02:26 -0800

    update

--- thetagang/test_options.py
+++ thetagang/test_options.py
@@ -2,7 +2,7 @@ from unittest.mock import MagicMock, patch
 
 from ib_async import Option, Ticker
 
-from thetagang.options import _price_is_valid
+from thetagang.options import _nearest_strikes, _price_is_valid
 from thetagang.types import OptionRight
 
 
@@ -44,3 +44,31 @@ def test_price_is_valid_put_exceeds_market_price(_: MagicMock) -> None:
         contract=Option(strike=110),
     )
     assert _price_is_valid(OptionRight.PUT, ticker, 100, 0.05) is False
+
+
+def test_nearest_strikes_put() -> None:
+    strikes = [90.0, 95.0, 100.0, 105.0, 110.0]
+    strikes_per_chain = 3
+    result = _nearest_strikes(OptionRight.PUT, strikes_per_chain, strikes)
+    assert result == [100, 105, 110]
+
+
+def test_nearest_strikes_call() -> None:
+    strikes = [90.0, 95.0, 100.0, 105.0, 110.0]
+    strikes_per_chain = 3
+    result = _nearest_strikes(OptionRight.CALL, strikes_per_chain, strikes)
+    assert result == [90, 95, 100]
+
+
+def test_nearest_strikes_put_less_than_chain() -> None:
+    strikes = [90.0, 95.0]
+    strikes_per_chain = 3
+    result = _nearest_strikes(OptionRight.PUT, strikes_per_chain, strikes)
+    assert result == [90, 95]
+
+
+def test_nearest_strikes_call_less_than_chain() -> None:
+    strikes = [90.0, 95.0]
+    strikes_per_chain = 3
+    result = _nearest_strikes(OptionRight.CALL, strikes_per_chain, strikes)
+    assert result == [90, 95]
